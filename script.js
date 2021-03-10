1d17e1238d0b2fc6399b5a015c69471f5fbe81da
robinucar <mehmet.ucar@imediapro.co.uk>
2021-03-10 15:39:42 +0000

    Output Users - forEach () & DOM Methods

--- script.js
+++ script.js
@@ -8,12 +8,11 @@ const calculateWealthBtn = document.getElementById('calculate-wealth');
 let data = [];
 
 // fetch random user and add money
-getRandomUser();
-getRandomUser();
-getRandomUser();
+
 const getRandomUser = async () => {
   const res = await fetch('https://randomuser.me/api');
   const data = await res.json();
+
   const user = data.results[0];
   const newUser = {
     name: `${user.name.first} ${user.name.last}`,
@@ -26,4 +25,31 @@ const getRandomUser = async () => {
 
 const addData = (obj) => {
   data.push(obj);
+  updateDom();
 };
+
+//updateDom
+
+const updateDom = (provideData = data) => {
+  //clear main div
+  main.innerHTML = ' <h2><strong>Person</strong>Wealth</h2>';
+
+  provideData.forEach((item) => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+};
+
+//format number as money
+
+const formatMoney = (number) => {
+  return '$ ' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+};
+
+//Event Listeners
+
+addUserBtn.addEventListener('click', getRandomUser);
